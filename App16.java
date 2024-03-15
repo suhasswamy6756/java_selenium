e602aefd27ea5b76a19fb2714505bef1c8b2318c
Suhas k s <109537992+suhasswamy6756@users.noreply.github.com>
2024-03-15 09:42:37 +0530

    Update App16.java

--- App16.java
+++ App16.java
@@ -1,18 +1,51 @@
 import java.time.Duration;
-import org.openqa.selenium.Alert;
+
+//import org.openqa.selenium.Alert;
+
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
+//import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
-public class App16{
-    public static void main(String[] args) throws InterruptedException{
-       System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ankitha A Kantli\\Desktop\\aecc\\testing\\src\\drivers\\chromedriver.exe");
-       WebDriver driver=new ChromeDriver();
-       driver.get("https://store.steampowered.com/news/");
-       new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(new By.ByXPath("//div[starts-with(@class, 'eventcalendar_FilterSettings')]//div[contains(., 'Options and Filters')]"))).click();
-       Thread.sleep(4000);
-       driver.quit();
-    }
+
+public class prg_9_16{
+
+ 
+public static void main(String[] args) throws InterruptedException{
+
+ 
+System.setProperty("webdriver.chrome.driver", 
+
+"C:\\Users\\saish\\OneDrive\\Desktop\\computer science\\javapro\\Software 
+
+Testing\\demo\\src\\drivers\\chromedriver.exe");
+
+ 
+WebDriver driver=new ChromeDriver();
+
+ 
+driver.get("https://store.steampowered.com/news/");
+
+ 
+new WebDriverWait(driver, 
+
+Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(new 
+
+By.ByXPath("/html/body/div[1]/div[7]/div[6]/div[3]/div/div/div[1]/div/div[3]/div[6]/div"))).click();
+
+ 
+Thread.sleep(400000);
+
+ 
+driver.quit();
+
+ 
 }
+
+    
